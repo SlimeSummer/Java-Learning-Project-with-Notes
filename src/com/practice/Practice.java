a309359a620cc53221cd095965e9d90271d826a8
SlimeSummer <2684809516@qq.com>
2025-07-14 20:34:46 +0800

    update

--- src/com/practice/Practice.java
+++ src/com/practice/Practice.java
@@ -2,6 +2,16 @@ package com.practice;
 
 public class Practice {
     public static void main (String[] args) {
-        
+        int sum=0;
+        for (int i=0; i<5; i++){
+            sum+=i;
+            System.out.println("i:"+ i);
+            System.out.println("sum:"+ sum);
+        }
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
     }
 }
